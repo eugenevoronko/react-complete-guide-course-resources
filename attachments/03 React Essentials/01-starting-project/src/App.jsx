3aa10ce33135fdc57effd6d9098901b9fc18c124
Eugene <yvoro@softserveinc.com>
2025-07-16 18:02:14 +0300

    office commit

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,25 +1,33 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genereateRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
+    const description = reactDescriptions[genereateRandomInt(2)];
+
+    return (
+        <header>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {} React concepts you will need for almost any app you are going
+                to build!
+            </p>
+        </header>
+    );
 }
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
