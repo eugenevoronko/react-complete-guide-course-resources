905fb8d98d8f43463e4dde329aacf4b66313f723
Eugene V <34031302+eugenevoronko@users.noreply.github.com>
2025-07-17 22:04:24 +0300

    home commit

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,33 +1,35 @@
+import reactImage from "/src/assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genereateRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-    const description = reactDescriptions[genereateRandomInt(2)];
+  const description = reactDescriptions[genereateRandomInt(2)];
 
-    return (
-        <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {} React concepts you will need for almost any app you are going
-                to build!
-            </p>
-        </header>
-    );
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <main>
-                <h2>Time to get started!</h2>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
 }
 
 export default App;
