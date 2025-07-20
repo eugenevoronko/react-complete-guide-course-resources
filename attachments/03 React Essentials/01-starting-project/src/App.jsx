39531afb00eedd82de46a7c6610c5f8734ce298e
Eugene <yvoro@softserveinc.com>
2025-07-20 11:52:49 +0300

    office commit

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,46 +1,62 @@
+import React, { useState } from "react";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data-with-examples.js";
 
 function App() {
-  function handleSelect(selectedButton) {
-    console.log(selectedButton);
-  }
+    const [selectedButton, setSelectedButton] = useState("components");
 
-  return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              image={CORE_CONCEPTS[0].image}
-              description={CORE_CONCEPTS[0].description}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("Components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("States")}>
-              States
-            </TabButton>
-          </menu>
-        </section>
-      </main>
-    </div>
-  );
+    function handleSelect(selectedButton) {
+        setSelectedButton(selectedButton);
+        console.log(selectedButton);
+    }
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            image={CORE_CONCEPTS[0].image}
+                            description={CORE_CONCEPTS[0].description}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect("components")}>
+                            Components
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("jsx")}>
+                            JSX
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("props")}>
+                            Props
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("state")}>
+                            State
+                        </TabButton>
+                    </menu>
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedButton].title}</h3>
+                        <p>{EXAMPLES[selectedButton].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedButton].code}</code>
+                        </pre>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
